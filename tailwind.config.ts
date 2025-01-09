62602cf122dfd72aaecae1db3351b88393082c24
tevelsho <tevelshorz@gmail.com>
2025-01-09 21:53:38 +0800

    feat(theme): add extended colors to tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#5073b1",
+        second: "#5073b1",
+        accent: "#5073b1",
       },
     },
   },
