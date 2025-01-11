5c9f87714985aa09bde05c34b50099c08fde36bf
ctjh <64637198+ctjh@users.noreply.github.com>
2025-01-11 15:24:32 +0800

    Replaced placeholder questions with actual content

--- app/(pages)/faq/page.tsx
+++ app/(pages)/faq/page.tsx
@@ -13,37 +13,45 @@ const Faq:React.FC = () => {
             </h1>
             <div id = "accordion" className="w-[70%] mx-auto">
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "Can my team be composed of members from different schools?"
+                    answer= "Absolutely, as long as each teammate meets the eligibility criteria!"
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "Change I change the name and details of my team after registration?"
+                    answer= "The information submitted during registration is final!"
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "Should I participate even if I have not studied H2 or Olympiad level economics"
+                    answer= "Yes! All pre-U students, regardless of their subjects, can participate. SEL has questions of a wide range of difficulties, so you can challenge yourself regardless of your economics proficiency. !"
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "May I use the internet during this competition?"
+                    answer= "Yes! Online resources such as textbooks, research papers, graphing calculators and computing software can all be used to aid in solving questions.!"
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "How do we obtain new problems to solve during the contest?"
+                    answer= "After completing or skipping an existing question, your team can choose a new questions from a pool of subsequent questions. You can attempt a maximum of 4 main problems at any one time."
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "What is the difficulty of SEL questions?"
+                    answer= "SEL questions range in difficulty from those similar to the ones you have encountered in school, to questions set at the undergraduate and olympiad level."
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "What happens if I submit a wrong answer?"
+                    answer= "you can reattempt a question-part as many times as you need, but the points awarded for a correct answer will diminish. Certain questions such as MCQs have a limit of 3 attempts."
                 />
                 <FaqBox
-                    question = "Can my teammates be from other schools?"
-                    answer= "Absolutely!"
+                    question = "May I skip questions?"
+                    answer= "Yes, but this can incur a penalty of 2 points. Your team will receive a free skip that allows you to skip a question without penalty every 30 minutes."  
                 />
+                 <FaqBox
+                    question = "How do I obtain bonus points?"
+                    answer= "From 10 am, the Economania section will be unlocked, featuring 5 categories of 3 questions each in ascending difficulty. If your team completes some or all categories of Economania before 11 am, the points obtained from those categories will be doubled."
+                />
+                 <FaqBox
+                    question = "Where can I send my enquiries about SEL?"
+                    answer= "Please email contact@sgeconsleague.org, and we will tend to any further questions you may have!"
+                    />
             </div>
         </section>
         </main>
