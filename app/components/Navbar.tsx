3f5cdb860d77c0402b9b107c90195d387d626e3e
CheeGuang <123812015+CheeGuang@users.noreply.github.com>
2025-02-19 02:18:02 +0800

    added sample qn page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"; // Import Next.js Link
 
 const Navbar: React.FC = () => {
   return (
-    <div className="w-full px-8 lg:px-20 bg-gradient-to-b from-[#0035C71A] to-[#DEE7FB] pb-12"> 
-      <nav className="flex items-center justify-between py-12 max-w-[1500px] mx-auto w-full"> 
+    <div className="w-full px-8 lg:px-20 bg-gradient-to-b from-[#0035C71A] to-[#DEE7FB] pb-12">
+      <nav className="flex items-center justify-between py-12 max-w-[1500px] mx-auto w-full">
         {/* SEL Logo - Clickable */}
         <div className="flex items-center">
           <Link href="/">
@@ -21,7 +21,10 @@ const Navbar: React.FC = () => {
           <Link href="/rules" className="hover:text-[#4CA9DF] transition">
             Rules
           </Link>
-          <Link href="#sample-questions" className="hover:text-[#4CA9DF] transition">
+          <Link
+            href="/sample-questions"
+            className="hover:text-[#4CA9DF] transition"
+          >
             Sample Questions
           </Link>
           <Link href="/faq" className="hover:text-[#4CA9DF] transition">
