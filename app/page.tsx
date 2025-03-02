7f2dcbab0f7336f087bb34a52d8b505f622fe0b5
tevelsho <tevelshorz@gmail.com>
2025-03-02 21:53:54 +0800

    feat: new arrows for faq and syllabus box

--- app/page.tsx
+++ app/page.tsx
@@ -571,7 +571,6 @@ export default function HomePage() {
                 <span>Join the SEL Discord Server!</span>
               </a>
             </div>
-
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4 text-lg">
               <div
                 className="
@@ -608,7 +607,24 @@ export default function HomePage() {
                       hover:scale-105
                     "
                   >
-                    Read Our FAQ 🡒
+                    Read Our FAQ
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right inline-block ml-2 w-5 h-5"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5 12l14 0" />
+                      <path d="M13 18l6 -6" />
+                      <path d="M13 6l6 6" />
+                    </svg>
                   </a>
                 </div>
                 <Image
@@ -666,7 +682,24 @@ export default function HomePage() {
                       hover:scale-105
                     "
                   >
-                    View the Syllabus 🡒
+                    View the Syllabus
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right inline-block ml-2 w-5 h-5"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5 12l14 0" />
+                      <path d="M13 18l6 -6" />
+                      <path d="M13 6l6 6" />
+                    </svg>
                   </a>
                 </div>
                 <Image
