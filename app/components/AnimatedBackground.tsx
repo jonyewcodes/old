ce451104d455abada439afd989e29a6f8bcde611
tevelsho <tevelshorz@gmail.com>
2025-03-05 14:38:49 +0800

    fix: animation background lines

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 export default function AnimatedBackground() {
   return (
-    <section className="relative w-full h-[600px] overflow-hidden">
-      <div className="absolute top-0 left-0 w-full h-full z-0 scrolling-bg" />
+    <div className="absolute inset-0 z-0 overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full scrolling-bg" />
       <style jsx>{`
         .scrolling-bg {
-          background-image: url("/animation.png"); 
-          background-repeat: repeat-x; 
-          background-size: auto 100%; 
-          width: 200%;  
+          background-image: url("/animation.png");
+          background-repeat: repeat-x;
+          background-size: auto 100%;
+          width: 200%;
           height: 100%;
           animation: scroll-bg 80s linear infinite;
-          animation-timing-function: linear; 
+          animation-timing-function: linear;
         }
 
         @keyframes scroll-bg {
@@ -25,6 +25,6 @@ export default function AnimatedBackground() {
           }
         }
       `}</style>
-    </section>
+    </div>
   );
 }
